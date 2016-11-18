b1a59de6e32433a3f7d0189f9c104ff95cfc56cf
Alivia Blount <aliviablount@gmail.com>
2016-11-18 13:37:19 -0800

    devise gem

---
+++ db/migrate/20161118211440_devise_create_gaurdians.rb
@@ -0,0 +1,53 @@
+class DeviseCreateGaurdians < ActiveRecord::Migration[5.0]
+  def change
+    create_table :gaurdians do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.string :full_address
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+      t.string :country
+      t.string :latitude
+      t.string :longitude
+
+      t.timestamps null: false
+    end
+
+    add_index :gaurdians, :email,                unique: true
+    add_index :gaurdians, :reset_password_token, unique: true
+    # add_index :gaurdians, :confirmation_token,   unique: true
+    # add_index :gaurdians, :unlock_token,         unique: true
+  end
+end
