12dcfb72987326013fe329851c29df1560272477
Alivia Blount <aliviablount@gmail.com>
2016-11-22 10:38:45 -0800

    in progress select tag

--- app/controllers/providers/registrations_controller.rb
+++ app/controllers/providers/registrations_controller.rb
@@ -1,22 +1,17 @@
 class Providers::RegistrationsController < Devise::RegistrationsController
+  include ApplicationHelper
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
-before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  #GET /resource/sign_up
+  def new
+    super
+    build_resource()
+    # self.resource = Provider.new(sign_in_params)
+    @center_options = Center.all.collect {|c| [ c.name, c.id ] }
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |provider_params|
-      provider_params.permit(:email, :name, :phone, :full_address, :city, :state, :zipcode, :center_id, :neighborhood_id)
-    end
   end
-
-
-
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
 
   # POST /resource
   # def create
@@ -68,4 +63,12 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |provider_params|
+      provider_params.permit(:email, :name, :phone, :full_address, :city, :state, :zipcode, :center_id, :neighborhood_id)
+    end
+  end
 end
