750f8dc4f57a3638272cb197358b7929b9fcdc5d
Alivia Blount <aliviablount@gmail.com>
2016-11-28 20:11:22 -0800

    update guardian show view

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -3,5 +3,6 @@ class ProvidersController < ApplicationController
   	@provider = Provider.find(params[:id])
   	@center = Center.find(@provider.center_id)
   	@cfgp = ChildFamilyGuardianProvider.last
+  	@guardian = Guardian.find(@cfgp.guardian_id)
   end
 end
---
+++ app/views/guardians/_guardian.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! guardian, :id, :index, :show, :created_at, :updated_at
+json.url guardian_url(guardian, format: :json)
