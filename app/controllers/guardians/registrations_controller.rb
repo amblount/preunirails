efd63423bb710ca63b852e33f25362571444cd09
Alivia Blount <aliviablount@gmail.com>
2016-11-30 14:28:30 -0800

    remove configure permitted paramters from guardians controller, sign_our current_guardian

--- app/controllers/guardians/registrations_controller.rb
+++ app/controllers/guardians/registrations_controller.rb
@@ -2,11 +2,11 @@ class Guardians::RegistrationsController < Devise::RegistrationsController
 include ApplicationHelper
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
-before_action :configure_permitted_parameters, if: :devise_controller?
+# before_action :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_up
   def new
-
+    @center_options = Center.all.collect {|c| [ c.name, c.id ] }
     @neighborhood_options = Neighborhood.all.collect {|n| [ n.name, n.id ] }
     @guardian = build_resource()
     super
@@ -14,7 +14,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   # POST /resource
   def create
-    build_resource(configure_permitted_parameters)
+    build_resource(configure_sign_up_params)
 
     super
   end
