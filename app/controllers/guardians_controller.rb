7e4ea6be747fffa81614b25af4f570d77b9b5378
Alivia Blount <aliviablount@gmail.com>
2016-12-09 01:55:09 -0800

    clean up guardian show

--- app/controllers/guardians_controller.rb
+++ app/controllers/guardians_controller.rb
@@ -1,5 +1,8 @@
 class GuardiansController < ApplicationController
   def show
     @guardian = current_guardian
+    @children = @guardian.children
+    @famillies = @guardian.families
+    @centers = Center.all.limit(4)
   end
 end
